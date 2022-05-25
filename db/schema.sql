fce68aeb0bf234aa5fb6caa8210a0429a8c3eda3
Colin Brooks <mcbrooks@pm.me>
2022-05-24 23:41:30 -0400

    updated db files

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE departments (
-    id INT,
+    id INT AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO departments (id, name)
-VALUES  (00, "C-Suite"),
-        (01, "Portfolio Management"),
-        (02, "Credit Research");
+VALUES  (1, "C-Suite"),
+        (2, "Portfolio Management"),
+        (3, "Credit Research");
 
 INSERT INTO roles (id, title, salary, department_id)
-VALUES  (000, "Chief Executive Officer", 2200000, 00),
-        (001, "Chief Financial Officer", 1750000, 00), 
-        (002, "Chief Compliance Officer", 1500000, 00),
-        (003, "Senior Vice President", 250000, 01),
-        (004, "Vice President", 150000, 01), 
-        (005, "Credit Manager", 120000, 02),
-        (006, "Senior Credit Analyst", 85000, 02),
-        (007, "Credit Analyst", 70000, 02),
-        (008, "Junior Credit Analyst", 50000, 02),
-        (009, "Credit Intern", 25000, 02);
+VALUES  (1, "Chief Executive Officer", 2200000, 1),
+        (2, "Chief Financial Officer", 1750000, 1), 
+        (3, "Chief Compliance Officer", 1500000, 1),
+        (4, "Senior Vice President", 250000, 2),
+        (5, "Vice President", 150000, 2), 
+        (6, "Credit Manager", 120000, 3),
+        (7, "Senior Credit Analyst", 85000, 3),
+        (8, "Credit Analyst", 70000, 3),
+        (9, "Junior Credit Analyst", 50000, 3),
+        (10, "Credit Intern", 25000, 3);
 
 INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES  (000, "Patrick", "Bateman", 000, 000),
-        (001, "Jordon", "Belfort", 001, 000), 
-        (002, "Paul", "Allen", 002, 000),
-        (003, "Robert", "Downey, Jr.", 003, 001),
-        (004, "Joseph", "Bergstein", 004, 001), 
-        (005, "Alex", "Monte-Carlo", 005, 001),
-        (006, "Robert", "Scholes", 006, 005),
-        (007, "Werner", "Von Braun", 007, 005),
-        (008, "David", "Schrodinger", 008, 005),
-        (009, "Guy", "Morales", 009, 005);
+VALUES  (1, "Patrick", "Bateman", 1, 1),
+        (2, "Jordon", "Belfort", 2, 1), 
+        (3, "Paul", "Allen", 3, 1),
+        (4, "Robert", "Downey, Jr.", 4, 2),
+        (5, "Joseph", "Bergstein", 5, 2), 
+        (6, "Alex", "Monte-Carlo", 6, 2),
+        (7, "Robert", "Scholes", 7, 6),
+        (8, "Werner", "Von Braun", 8, 6),
+        (9, "David", "Schrodinger", 9, 6),
+        (10, "Guy", "Morales", 10, 6);
