45bb22e3cbf45d6944597185c69b7a5bb95737be
Colin Brooks <mcbrooks@pm.me>
2022-05-31 20:45:13 -0400

    completed add employee question

--- index.js
+++ index.js
@@ -103,12 +103,12 @@ function mainPrompt() {
         // Add A Role
         if (answers.main_menu === "Add A Role") {
             var dept_choices = []
-            db.query('SELECT name FROM departments', function (err, results) {
+            db.query('SELECT * FROM departments', function (err, results) {
                 if (err) {
                     console.log(err)
                     }
                 for (let i = 0; i < results.length; i++) {
-                    dept_choices.push(results[i])
+                    dept_choices.push(results[i].name)
                 }
             })
             
@@ -133,10 +133,8 @@ function mainPrompt() {
             // Prompt role related questions & prints roles
             inquirer.prompt(role_prompt)
             .then((answers) => {
-                console.log(dept_choices)
-                console.log(answers.role_dept)
-                let index = dept_choices.indexOf("'" + answers.role_dept + "'")
-                var sql = ('INSERT INTO roles (title, salary, deptartment_id) VALUES ("' + answers.role_name + '", ' + answers.role_salary + ', ' + index + ')')
+                let index = dept_choices.indexOf(answers.role_dept)
+                var sql = ('INSERT INTO roles (title, salary, department_id) VALUES ("' + answers.role_name + '", ' + answers.role_salary + ', ' + index + ')')
                 db.query(sql, function (err, results) {
                     if (err) {
                         console.log(err)
@@ -153,7 +151,77 @@ function mainPrompt() {
                 })            
                     
             })
-        }  
+        }
+        // Add an Employee
+        if (answers.main_menu === "Add An Employee") {
+            var role_choices = []
+            var mgr_choices = []
+            db.query('SELECT * FROM roles', function (err, results) {
+                if (err) {
+                    console.log(err)
+                    }
+                for (let i = 0; i < results.length; i++) {
+                    role_choices.push(results[i].title)
+                }
+            })
+            db.query('SELECT * FROM employees', function (err, results) {
+                if (err) {
+                    console.log(err)
+                    }
+                for (let i = 0; i < results.length; i++) {
+                    let mgr_first = results[i].first_name
+                    let mgr_last = results[i].last_name
+                    mgr_choices.push((mgr_first + ' ' + mgr_last))
+                }
+            })
+            
+            const employee_prompt = [
+                {
+                    name: 'first_name',
+                    type: 'input',
+                    message: "What is the Employee's first name?"
+                },
+                {
+                    name: 'last_name',
+                    type: 'input',
+                    message: "What is the Employees's last name?"
+                },
+                {
+                    name: 'employee_role',
+                    type: 'list',
+                    message: "What is the Employees's role?",
+                    choices: role_choices
+                },
+                {
+                    name: 'employee_mgr',
+                    type: 'list',
+                    message: "Who is the Employee's manager?",
+                    choices: mgr_choices
+                },
+            ]
+            // Prompt employee related questions & prints employees
+            inquirer.prompt(employee_prompt)
+            .then((answers) => {
+                let role_answser = role_choices.indexOf(answers.employee_role)
+                let manager_id = mgr_choices.indexOf(answers.employee_mgr)
+                var sql = ('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("' + answers.first_name + '", "' + answers.last_name + '", ' + role_answser + ', ' + manager_id +')')
+                db.query(sql, function (err, results) {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
+                // Prints all roles and returns to main menu
+                db.query('SELECT * FROM employees', function (err, results) {
+                    if (err) {
+                        console.log(err)
+                    }
+                    const table = cTable.getTable(results)
+                    console.log(table)
+                    mainPrompt()
+                })            
+                    
+            })
+        }    
     })
 }
 
